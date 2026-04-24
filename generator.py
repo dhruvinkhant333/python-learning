37d2d212be8d31ae0c2d4010abc6ad3d38d332b6
dhruvinkhant333 <dhruvinkhant333@gmail.com>
2026-04-24 23:08:42 +0530

    second exersize of genrators : Write a generator called running_total that takes a list of numbers and yields the running sum at each step:

--- generator.py
+++ generator.py
@@ -7,4 +7,13 @@ def even_numbers(n):
 for num in even_numbers(10):
     print(num)  # Output: 0, 2, 4, 6, 8, 10
 
+# TASK 2 : Write a generator called running_total that takes a list of numbers and yields the running sum at each step:
 
+def running_total(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+        yield total
+# Example usage:
+for total in running_total([1, 2, 3, 4]):
+    print(total)  # Output: 1, 3, 6, 10
