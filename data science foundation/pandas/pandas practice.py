76f8274a66c8f30742dedb804cde510c55642655
Leo <dhruvinkhant333@gmail.com>
2026-08-07 23:45:50 +0530

    learning pandas basic consepts also added one helpfull book in this python journey to understand machine learning path easily

--- data science foundation/pandas/pandas practice.py
+++ data science foundation/pandas/pandas practice.py
@@ -9,6 +9,26 @@ arr = np.array([10,20,30,40,50])
 s2 = pd.Series(arr)
 print(s2)
 
-s = pd.Series([100, 200, 300], index=['First', 'Second', 'Third'], name='Revenue')
-print(f"values : {s.values}")
-print(f"type : {type(s.values)}")
+print("""
+ ---------------------------------------------------------------------------- 
+                          series properties                              
+ ---------------------------------------------------------------------------- 
+""")
+s3 = pd.Series([100, 200, 300], index=['First', 'Second', 'Third'], name='Revenue')
+print(f"Series : {s3}")
+
+print(f"values : {s3.values}")
+print(f"type : {type(s3.values)}")
+print(f"dtype : {s3.dtype}")
+print(f"shape : {s3.shape}")
+
+
+print("""
+# ---------------------------------------------------------------------------- #
+#                           accesing series elements                           #
+# ---------------------------------------------------------------------------- #
+""")
+s4 = pd.Series([10, 20, 30, 40, 50], index=['a', 'b', 'c', 'd', 'e'])
+print(f"Series:\n{s4}")
+
+
